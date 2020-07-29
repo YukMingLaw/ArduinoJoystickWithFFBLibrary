45ee061d4e71ee2fea0028fa0f76bbae1a2398cf
yo_law <luoym@cloudvos.com>
2020-07-29 14:04:06 +0800

    fix example bug

--- examples/SingleJoystickFFB/SingleJoystickFFB.ino
+++ examples/SingleJoystickFFB/SingleJoystickFFB.ino
@@ -9,17 +9,19 @@ Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
 
 Gains mygains[2];
 EffectParams myeffectparams[2];
-
-//set X Axis gains
-mygains[0].totalGain = 100;//0-100
-mygains[0].springGain = 100;//0-100
+int32_t forces[2] = {0};
 
 void setup(){
     pinMode(A2,INPUT);
-    pinMode(9,INPUT);
+    pinMode(9,OUTPUT);
+    pinMode(6,OUTPUT);
+    pinMode(7,OUTPUT);
     Joystick.setXAxisRange(0, 1023);
     //Steering wheel
     //Joystick.setXAxisRange(-512, 512);
+    //set X Axis gains
+    mygains[0].totalGain = 100;//0-100
+    mygains[0].springGain = 100;//0-100
     //enable gains REQUIRED
     Joystick.setGains(mygains);
     Joystick.begin();
@@ -33,13 +35,23 @@ void loop(){
   //Steering wheel
   //myeffectparams[0].springMaxPosition = 512;
   //myeffectparams[0].springPosition = value - 512; //-512-512
-  
+
+  //Send HID data to PC
   Joystick.setXAxis(value);
+
+  //Recv HID-PID data from PC and caculate forces
   //Steering wheel
   //Joystick.setXAxis(value - 512);
   Joystick.setEffectParams(myeffectparams);
   Joystick.getForce(forces);
-
-  analogWrite(9,forces);
+  if(forces[0] > 0){
+    digitalWrite(6,LOW);
+    digitalWrite(7,HIGH);
+    analogWrite(9,abs(forces[0]));
+  }else{
+    digitalWrite(6,HIGH);
+    digitalWrite(7,LOW);
+    analogWrite(9,abs(forces[0]));
+  }
   delay(1);
 }
